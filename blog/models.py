c546421f933cc7e52e39d20e3f6695a064a198ad
YuriShepel <donbidonn@gmail.com>
2022-12-14 17:43:11 +0200

    Add text redactor to admin panel for posts

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateField()
-    content = models.TextField()
+    content = RichTextField(blank=True)
 
     def __str__(self):
         return self.title
