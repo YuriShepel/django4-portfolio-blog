21808766d9e8703e5771086b2c244e6833563b2a
YuriShepel <donbidonn@gmail.com>
2022-12-15 14:38:18 +0000

    Add /static/ to .gitignore

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lvn&na@&q1h@m-@(a5o4b5cn^n$hmjl8k#mw0f6odm&=b@0wx&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['donbidon.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
