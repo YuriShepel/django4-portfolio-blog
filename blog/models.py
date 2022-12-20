5cab96f1190ace9c2c3771cbe8e0e99fc421402c
YuriShepel <donbidonn@gmail.com>
2022-12-20 15:28:05 +0200

    Added plural name to blog/models Comments

--- blog/models.py
+++ blog/models.py
@@ -17,5 +17,9 @@ class Comments(models.Model):
     text = models.TextField('Comment', max_length=500)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    class Meta():
+        verbose_name = 'Comments'
+        verbose_name_plural = 'Comments'
+
     def __str__(self):
         return f'{self.name}, {self.post}'
