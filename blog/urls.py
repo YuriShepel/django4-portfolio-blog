5fe6a28b9a1997d8dbb14f63f3546b5ff28c0d95
YuriShepel <donbidonn@gmail.com>
2022-12-20 15:08:47 +0200

    Add comment form to blog

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.AllPosts.as_view(), name='all_blogs'),
     path('<int:post_id>/', views.Detail.as_view(), name='detail'),
+    path('rewiew/<int:pk>/', views.AddComments.as_view(), name='add_comments'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.base import View
 from .models import Post
+from .form import CommentsForm
 
 
 class AllPosts(View):
@@ -13,3 +14,13 @@ class Detail(View):
     def get(self, request, post_id):
         post = get_object_or_404(Post, pk=post_id)
         return render(request, 'blog/detail.html', {'post': post})
+
+
+class AddComments(View):
+    def post(self, request, pk):
+        form = CommentsForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.post_id = pk
+            form.save()
+        return redirect(f'/blog/{pk}')
